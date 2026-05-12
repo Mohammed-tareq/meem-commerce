ef1a49a01d0e54ee17e799941582a8ef5277a28b
mohammed tareq <mohtareq1999m@gmail.com>
2026-05-12 10:51:18 +0300

    refactor(ProductMiniResource): Enhance product data representation with additional fields

--- app/Http/Resources/Product/ProductMiniResource.php
+++ app/Http/Resources/Product/ProductMiniResource.php
@@ -12,8 +12,32 @@ class ProductMiniResource extends JsonResource
      *
      * @return array<string, mixed>
      */
-    public function toArray(Request $request): array
+
+    public function toArray($request): array
     {
-        return parent::toArray($request);
+        return [
+            'id'                     => $this->id,
+            'name'                   => $this->getTranslation('name', app()->getLocale()),
+            'slug'                   => $this->slug,
+            'price'                  => $this->roundMoney($this->price),
+            'current_price'          => $this->roundMoney($this->current_price),
+            'price_after_discount'    => $this->roundMoney($this->price_after_discount),
+            'discount_type'          => $this->discount_type,
+            'discount_amount'        => $this->discount_amount,
+            'quantity'               => $this->quantity,
+            'discount_valid'           =>  (bool) $this->isDiscountActive(),
+        ];
+    }
+
+
+
+
+    private function roundMoney($value)
+    {
+        if ($value === null || $value === '') {
+            return null;
+        }
+
+        return round((float) $value, 2);
     }
 }
