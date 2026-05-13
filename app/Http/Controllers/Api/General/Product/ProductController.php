e9f0a35412e661468723951bae40ab468e376a46
mohammed tareq <mohtareq1999m@gmail.com>
2026-05-13 12:46:31 +0300

    feat(ProductController): Implement product retrieval and pagination functionality

--- app/Http/Controllers/Api/General/Product/ProductController.php
+++ app/Http/Controllers/Api/General/Product/ProductController.php
@@ -3,10 +3,10 @@
 namespace App\Http\Controllers\Api\General;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Product\ProductResource;
 use App\Services\General\ProductService;
 use Illuminate\Http\Request;
 use \Marvel\Http\Resources\product\ProductCollection;
-use Marvel\Http\Resources\ProductResource;
 use Marvel\Traits\ApiResponse;
 
 class ProductController extends Controller
