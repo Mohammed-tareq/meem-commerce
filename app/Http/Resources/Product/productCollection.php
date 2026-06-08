2db2948dcf1aac00ca360342945bd89f69b2f8a5
mohammed tareq <mohtareq1999m@gmail.com>
2026-06-08 15:39:23 +0300

    feat: create ProductCollection resource to standardize paginated product responses

--- app/Http/Resources/Product/productCollection.php
+++ app/Http/Resources/Product/productCollection.php
@@ -8,26 +8,25 @@ use Illuminate\Http\Resources\Json\ResourceCollection;
 class ProductCollection extends ResourceCollection
 {
     /**
-     * Transform the resource into an array.
+     * Transform the resource collection into an array.
      *
-     * @param  Request  $request
-     * @return array
+     * @return array<int|string, mixed>
      */
-    public function toArray($request)
+    public function toArray(Request $request): array
     {
         return [
             "data" => ProductMiniResource::collection($this->collection),
             "links" => [
-                "current_page"   => $this->currentPage(),
-                "from"           => $this->firstItem(),
-                "to"             => $this->lastItem(),
-                "last_page"      => $this->lastPage(),
-                "path"           => $request->url(),
-                "per_page"       => $this->perPage(),
-                "total"          => $this->total(),
-                "next_page_url"  => $this->nextPageUrl(),
-                "prev_page_url"  => $this->previousPageUrl(),
-                "last_page_url"  => $this->url($this->lastPage()),
+                "current_page" => $this->currentPage(),
+                "from" => $this->firstItem(),
+                "to" => $this->lastItem(),
+                "last_page" => $this->lastPage(),
+                "path" => $request->url(),
+                "per_page" => $this->perPage(),
+                "total" => $this->total(),
+                "next_page_url" => $this->nextPageUrl(),
+                "prev_page_url" => $this->previousPageUrl(),
+                "last_page_url" => $this->url($this->lastPage()),
                 "first_page_url" => $this->url(1),
             ]
         ];
