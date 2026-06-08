b1d20095b51b3cc8c92685199cd2166fd041d4a2
mohammed tareq <mohtareq1999m@gmail.com>
2026-06-08 16:08:37 +0300

    Please provide the code changes or file diffs you would like me to summarize.

--- app/Http/Resources/Product/productCollection.php
+++ app/Http/Resources/Product/productCollection.php
@@ -4,31 +4,28 @@ namespace App\Http\Resources\Product;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection;
+use App\Http\Resources\Product\ProductMiniResource;
 
 class ProductCollection extends ResourceCollection
 {
-    /**
-     * Transform the resource collection into an array.
-     *
-     * @return array<int|string, mixed>
-     */
-    public function toArray(Request $request): array
+
+    public function toArray($request)
     {
-         return [
+        return [
             "data" => ProductMiniResource::collection($this->collection),
             "links" => [
-                "current_page" => $this->currentPage(),
-                "from" => $this->firstItem(),
-                "to" => $this->lastItem(),
-                "last_page" => $this->lastPage(),
-                "path" => $request->url(),
-                "per_page" => $this->perPage(),
-                "total" => $this->total(),
-                "next_page_url" => $this->nextPageUrl(),
-                "prev_page_url" => $this->previousPageUrl(),
-                "last_page_url" => $this->url($this->lastPage()),
+                "current_page"   => $this->currentPage(),
+                "from"           => $this->firstItem(),
+                "to"             => $this->lastItem(),
+                "last_page"      => $this->lastPage(),
+                "path"           => $request->url(),
+                "per_page"       => $this->perPage(),
+                "total"          => $this->total(),
+                "next_page_url"  => $this->nextPageUrl(),
+                "prev_page_url"  => $this->previousPageUrl(),
+                "last_page_url"  => $this->url($this->lastPage()),
                 "first_page_url" => $this->url(1),
             ]
         ];
     }
-}
+} 
