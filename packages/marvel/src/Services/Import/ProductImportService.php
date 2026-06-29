c06a3fbf891e597c40ac23ad01b8ea70f4c44c4a
mohammed tareq <mohtareq1999m@gmail.com>
2026-06-29 15:29:17 +0300

    feat: implement demo data seeding console command and product import background job system

--- packages/marvel/src/Services/Import/ProductImportService.php
+++ packages/marvel/src/Services/Import/ProductImportService.php
@@ -56,7 +56,7 @@ class ProductImportService
             $product = null;
 
             if (!empty($sku)) {
-                $product = Product::withTrashed()->where('sku', $sku)->first();
+                $product = Product::where('sku', $sku)->first();
             }
 
             $data = $this->buildProductData($row);
@@ -68,9 +68,7 @@ class ProductImportService
             }
 
             if ($product) {
-                if ($product->trashed()) {
-                    $product->restore();
-                }
+               
                 $data['slug'] = $product->slug;
                 $product->fill($data)->saveQuietly();
             } else {
